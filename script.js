3944a0f9a5b89abf6095447e8651393a156ee216
Fuzzymonkey <magne.jonassen@gmail.com>
2014-09-09 18:59:43 +0000

    Changes to js and added buttons in html.

--- script.js
+++ script.js
@@ -2,46 +2,104 @@
 var numberOfSquares = 256;
 
 // Default number of squares per row
-var squaresEachRow=16;
+var squaresEachRow = 16;
 
-// Makes 256(16x16 grid) squares on document ready (NEED TO CHANGE THIS SO IT CAN TAKE ARGUMENTS, LIKE THE DIFFERENT INTERACTIONS)
-$(document).ready(function(){
+//Interaction type
+var interactionType = "trail";
+
+//Set size of squares
+var width = ($(".grid-container").width()) / squaresEachRow;  
+
+// Makes 256(16x16 grid) squares on document ready (HOW TO MAKE THIS SMOOTHER? PAINFULLY SLOW ATM)
+var makeGrid = function(){
+  $(".grid-item").remove();
   for (var i = 1; i <= numberOfSquares; i++) {
-    $('.grid-container').append("<div></div>");
-    $('.grid-container > div').addClass('grid-item');
+    $('.grid-container').append("<div class='grid-item'></div>");
+    $('.grid-item').css("background-color", "#000000");
+    $('.grid-item').css({"width":width ,"height":width});
   }
+};
+
+//Creating of the grids
+//On page load
+$(document).ready(function(){
+    makeGrid();
 });
 
-//Adjust size of squares
+//Reset
 $(document).ready(function(){
-  var width = ($(".grid-container").width()) / squaresEachRow;  
-	$(".grid-item").css({"width":width ,"height":width});
+  $(".new-grid").click(function() {
+    makeGrid();
+  })
 });
 
-//Different types of interaction with the squares (NEED TO PUT THIS INTO sOME SORT OF IF/ELSE)
-// Trail
+
+//Trail
 $(document).ready(function(){
-  $('.grid-item').mouseenter(function(){
+  $(".trail").click(function() {
+    makeGrid();
+    var interactionType = "trail";
+  })
+});
+
+//Rdm colors
+$(document).ready(function(){
+  $(".random-colors").click(function() {
+    makeGrid();
+    var interactionType = "rdmColor";
+  })
+});
+
+
+//Opacity
+$(document).ready(function(){
+  $(".new-grid").click(function() {
+    makeGrid();
+    var interactionType = "opacity";
+  })
+});
+
+//Solid white
+$(document).ready(function(){
+  $(".solid-white").click(function() {
+    makeGrid();
+    var interactionType = "solid-white";
+  })
+});
+
+//Different types of interaction with the squares
+
+$(document).ready(function() {
+  if (interactionType === "trail") 
+  {
+    //Draw trail
+    $('.grid-item').mouseenter(function(){
     $(this).animate({backgroundColor:"#ffffff"}, 500);
   })
   $('.grid-item').mouseleave(function(){
     $(this).animate({backgroundColor:"#000000"}, 500);
   })
+  }
+  else if (interactionType === "rdmColor") 
+  {
+    //Draw with random colors
+    $('.grid-item').mouseenter(function(){
+      $(this).css("background-color", rdmColor());
+    })
+  }
+  else if (interactionType === "opacity")
+  {
+    //Draw with opacity
+    
+  }
+  else
+  {
+    //Draw with white
+    $('.grid-item').mouseenter(function(){
+      $(this).css("background-color", "#ffffff");
+    })
+  }
 });
-//Draw with white
-$(document).ready(function(){
-  $('.grid-item').mouseenter(function(){
-    $(this).css("background-color", "#ffffff");
-  })
-});
-//Draw with random colors
-$(document).ready(function(){
-  $('.grid-item').mouseenter(function(){
-    $(this).css("background-color", rdmColor());
-  })
-});
-//Each pass adds 10% black
-
 
 //Get random color values
 function rdmColor() {
@@ -50,3 +108,5 @@ function rdmColor() {
 	var blue = Math.floor((Math.random()*255)+1);
 	return "rgb(" + red + "," + green + "," + blue + ")";
 }
+  
+
